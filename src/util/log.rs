564d62fee0a1a039e0971754126bd373439d4cf7
Erik Jakobsson <erik.jakobsson@realsprint.com>
2025-11-07 16:48:22 +0100

    dont repeat code

--- src/util/log.rs
+++ src/util/log.rs
@@ -6,7 +6,7 @@ use vsprintf::vsprintf;
 use crate::ffi::*;
 use crate::sys::va_list;
 
-#[cfg(any(target_os = "macos", target_arch = "aarch64"))]
+#[cfg(any(target_os = "macos", target_os = "windows", target_arch = "aarch64"))]
 unsafe extern "C" fn callback(ptr: *mut c_void, level: c_int, fmt: *const c_char, args: va_list) {
 	if av_log_get_level() <= level {
 		return;
@@ -85,47 +85,6 @@ unsafe extern "C" fn callback(ptr: *mut c_void, level: c_int, fmt: *const c_char
 		_ => {}
 	};
 }
-
-#[cfg(target_os = "windows")]
-unsafe extern "C" fn callback(ptr: *mut c_void, level: c_int, fmt: *const c_char, args: va_list) {
-	if av_log_get_level() <= level {
-		return;
-	};
-
-	let mut line: &mut [u8] = &mut [0; 1024];
-	let mut print_prefix = 0;
-
-	let result = ffmpeg_sys::av_log_format_line2(
-		ptr,
-		level,
-		fmt,
-		args,
-		line.as_mut_ptr() as *mut c_char,
-		line.len() as c_int,
-		&mut print_prefix,
-	);
-
-	let line_length = (line.len() - 1).min(result as usize);
-
-	let Ok(string) = std::str::from_utf8(&line[..line_length]) else {
-		let string = CStr::from_ptr(fmt);
-		let string = string.to_str().unwrap_or_default();
-		tracing::warn!("invalid log line: {}", string);
-		return;
-	};
-
-	let string = string.trim();
-
-	match level {
-		AV_LOG_PANIC | AV_LOG_FATAL | AV_LOG_ERROR => tracing::error!("{string}"),
-		AV_LOG_WARNING => tracing::warn!("{string}"),
-		AV_LOG_INFO => tracing::info!("{string}"),
-		AV_LOG_VERBOSE | AV_LOG_DEBUG => tracing::debug!("{string}"),
-		AV_LOG_TRACE => tracing::trace!("{string}"),
-		_ => {}
-	};
-}
-
 
 pub fn register() {
 	unsafe {
